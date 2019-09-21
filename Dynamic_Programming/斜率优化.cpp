236a57339d141478b6a181689eb30cae783e880c
cycleke <cycleke@gmail.com>
2019-09-21 11:01:01 +0800

    Update My Templete

---
+++ Dynamic_Programming/斜率优化.cpp
@@ -0,0 +1,70 @@
+// 树上斜率优化
+// 定义dpi 表示i节点传递到根节点的最短耗时，规定dproot=−P。
+// 有如下转移方程dpu=dpv+dist(u,v)^2+P,v is an ancestor of u.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+const int MAXN = 1e5 + 5;
+
+vector<pii> adj[MAXN];
+ll dp[MAXN], d[MAXN];
+int n, p, q[MAXN], head, tail;
+
+inline ll S(int a, int b) { return (d[b] - d[a]) << 1; }
+inline ll G(int a, int b) { return dp[b] - dp[a] + d[b] * d[b] - d[a] * d[a]; }
+
+void dfs(int u, int from) {
+  vector<int> dhead, dtail;
+  if (u ^ 1) {
+    while (head + 2 <= tail &&
+           S(q[head + 1], q[head]) * d[u] <= G(q[head + 1], q[head]))
+      dhead.push_back(q[head++]);
+    int v = q[head];
+    dp[u] = dp[v] + p + (d[u] - d[v]) * (d[u] - d[v]);
+  }
+  while (head + 2 <= tail &&
+         G(u, q[tail - 1]) * S(q[tail - 1], q[tail - 2]) <=
+             G(q[tail - 1], q[tail - 2]) * S(u, q[tail - 1]))
+    dtail.push_back(q[--tail]);
+  q[tail++] = u;
+  for (pii &e : adj[u]) {
+    if (e.first == from) continue;
+    d[e.first] = d[u] + e.second;
+    dfs(e.first, u);
+  }
+  --tail;
+  for (int i = dtail.size() - 1; ~i; --i) q[tail++] = dtail[i];
+  for (int i = dhead.size() - 1; ~i; --i) q[--head] = dhead[i];
+}
+
+void solve() {
+  cin >> n >> p;
+  for (int i = 1; i <= n; ++i) adj[i].clear();
+  for (int i = 1, u, v, w; i < n; ++i) {
+    cin >> u >> v >> w;
+    adj[u].emplace_back(v, w);
+    adj[v].emplace_back(u, w);
+  }
+  dp[1] = -p;
+  head = tail = 0;
+  dfs(1, 1);
+
+  ll ans = 0;
+  for (int i = 1; i <= n; ++i)
+    if (dp[i] > ans) ans = dp[i];
+  cout << ans << '\n';
+}
+
+int main() {
+  // freopen("in.txt", "r", stdin);
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int o_o;
+  for (cin >> o_o; o_o; --o_o) solve();
+
+  return 0;
+}
--- Math/gauss.cpp
+++ Math/gauss.cpp
@@ -1,15 +1,16 @@
-const double EPS = 1e−9;
-const int MAXN = 220;
+
+const double EPS = 1e-9;
+const int MAXN = MAX_NODE;
 double a[MAXN][MAXN], x[MAXN];
 int equ, var;
 
-int Gauss() {
+int gauss() {
   int i, j, k, col, max_r;
   for (k = 0, col = 0; k < equ && col < var; k++, col++) {
     max_r = k;
     for (i = k + 1; i < equ; i++)
       if (fabs(a[i][col]) > fabs(a[max_r][col])) max_r = i;
-    if (fabs(a[max_r][col]) < eps) return 0;
+    if (fabs(a[max_r][col]) < EPS) return 0;
 
     if (k != max_r) {
       for (j = col; j < var; j++) swap(a[k][j], a[max_r][j]);
@@ -20,12 +21,23 @@ int Gauss() {
     for (j = col + 1; j < var; j++) a[k][j] /= a[k][col];
     a[k][col] = 1;
 
-    for (i = 0; i < equ; i++)
+    for (i = k + 1; i < equ; i++)
       if (i != k) {
-        x[i] − = x[k] * a[i][col];
-        for (j = col + 1; j < var; j++) a[i][j] − = a[k][j] * a[i][col];
+        x[i] -= x[k] * a[i][col];
+        for (j = col + 1; j < var; j++) a[i][j] -= a[k][j] * a[i][col];
         a[i][col] = 0;
       }
   }
+
+  for (col = equ - 1, k = var - 1; ~col; --col, --k) {
+    if (fabs(a[col][k]) > 0) {
+      for (i = 0; i < k; ++i) {
+        x[i] -= x[k] * a[i][col];
+        for (j = col + 1; j < var; j++) a[i][j] -= a[k][j] * a[i][col];
+        a[i][col] = 0;
+      }
+    }
+  }
+
   return 1;
 }
