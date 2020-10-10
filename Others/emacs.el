eb954d6afe565529a0740ddd9e714fc94804e13e
cycleke <cycleke@gmail.com>
2020-10-10 20:59:00 +0800

    修改emacs和vim配置

--- Others/emacs.el
+++ Others/emacs.el
@@ -1,18 +1,38 @@
-(defun comp ()
-  (interactive)
-  (save-some-buffers t)
-  (setq filename (file-name-nondirectory buffer-file-name))
-  (setq progname (file-name-sans-extension filename))
-  (setq suffix (file-name-extension filename))
-  (compile (concat "g++ " filename " -o " progname " -std=gnu++14 -O2 -Wall -Werror")))
-(add-hook 'c++-mode
-          '(lambda ()
-             (c-set-style "K&R")
-             (setq tab-width 2)
-             (setq indent-tabs-mode nil)
-             (setq c-basic-offset 2)))
-(global-set-key [f5] 'comp)
+(defun myc++ ()
+  (c-set-style "stroustrup")
+  (setq tab-width 2)
+  (setq indent-tabs-mode nil)
+  (setq c-basic-offset 2)
+  (c-toggle-hungry-state)
+  (defun compile-and-run()
+    (interactive)
+    (defvar file-name (file-name-sans-extension (file-name-nondirectory buffer-file-name)))
+    (compile
+     (format "g++ %s.cpp -o %s -Wall -Wextra -Wshadow -O2 && %s.exe"
+             file-name file-name file-name)))
+  (local-set-key (kbd "C-c C-c") 'compile-and-run)
+  (local-set-key (kbd "C-c C-k") 'kill-compilation))
+(add-hook 'c++-mode-hook 'myc++)
+
+(global-set-key [(meta ?o)] 'other-window)
+(global-set-key [(meta ?/)] 'hippie-expand)
+(global-set-key [(control tab)] ' senator-completion-menu-popup)
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+        try-expand-dabbrev-visible
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-all-abbrevs
+        try-expand-list
+        try-expand-line))
+
+(setq auto-save-mode nil)
+(setq make-backup-files nil)
 
 (ido-mode t)
+(show-paren-mode 1)
 (delete-selection-mode t)
+(global-linum-mode t)
 (global-auto-revert-mode t)
